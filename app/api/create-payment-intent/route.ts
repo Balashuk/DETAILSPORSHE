da8ea999a7dd16536004afb1c5332d4cb978a0a7
Yaroslav Balashuk <jaoslav.balashuk@gmail.com>
2025-01-26 19:53:12 +0200

    first commitssssss

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -8,7 +8,7 @@ import { getCurrentUser } from "@/actions/getCurrentUser";
 
 
 const stripe= new Stripe(process.env.STRIPE_SECRET_KEY as string,{
-    apiVersion:"2024-10-28.acacia"
+    apiVersion:"2024-12-18.acacia"
 });
 
 const calculateOrderAmount =(items:CartProductType[])=>{
